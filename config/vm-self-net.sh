cca89148ed6c2fd8e22405188b84a27eaaf421fe
Michael Mullin <masmullin@gmail.com>
2021-06-19 21:18:50 -0400

    better finding of en*

--- config/vm-self-net.sh
+++ config/vm-self-net.sh
@@ -3,7 +3,7 @@
 # matches the 1 to 3 digit ip octet value. $IPRH.$IPRH.$IPRH.$IPRH should find an ipaddress
 IPRH="\([[:digit:]]\{1,3\}\)"
 
-LINK=$(ip link | awk '{ print $2; }' | grep ^e | sed 's/://g')
+LINK=$(ip link | grep -E "^[[:digit:]]" | awk '{ print $2; }' | grep ^e | sed 's/://g')
 IPADD=$(ip a show $LINK | grep ether | awk '{ print $2 }' | sed 's/^[[:xdigit:]]*:[[:xdigit:]]*://g')
 FINALIPDIG=$(printf '%d' '0x'`ip a show $LINK | grep ether | awk '{ print $2 }' | sed 's/^[[:xdigit:]]*:[[:xdigit:]]*\(.*\)[[:xdigit:]]\{1,2\}://g'`)
 IPADD=$(printf '%d.%d.%d.%d' `echo $IPADD | sed 's/://g' | sed 's/\(..\)/0x\1 /g'`)
